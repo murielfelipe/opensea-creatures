f8ca168b7a7ac6f9526abff7ea766003074e896d
Felipe Muriel <murielfelipe@hotmail.com>
2021-01-24 10:46:56 -0500

    Test file

--- test/test-creature.js
+++ test/test-creature.js
@@ -27,7 +27,7 @@ contract("Creature", accounts => {
           .then(name => {
               assert.equal(
               name,
-              "Creature",
+              "FMATOken",
               "name is not creature"
               );
           });
